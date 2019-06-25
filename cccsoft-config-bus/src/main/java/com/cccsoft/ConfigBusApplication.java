1aef07eecb6bcd1c47924a947e496db54beedefc
zhangshun <shun.zhang@baozun.com>
2019-06-25 17:53:10 +0800

    添加config-server 和config-server

--- cccsoft-config-bus/src/main/java/com/cccsoft/ConfigBusApplication.java
+++ cccsoft-config-bus/src/main/java/com/cccsoft/ConfigBusApplication.java
@@ -2,15 +2,12 @@ package com.cccsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
-@EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigClientApplication {
+public class ConfigBusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
+		SpringApplication.run(ConfigBusApplication.class, args);
 	}
 
 }

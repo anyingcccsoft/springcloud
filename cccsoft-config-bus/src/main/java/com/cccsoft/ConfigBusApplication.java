fe3a44156642b7f7c173710ba87dac0453e4ac76
zhangshun <shun.zhang@baozun.com>
2019-07-17 15:39:48 +0800

    springcloud搭建学习

--- cccsoft-config-bus/src/main/java/com/cccsoft/ConfigBusApplication.java
+++ cccsoft-config-bus/src/main/java/com/cccsoft/ConfigBusApplication.java
@@ -2,8 +2,10 @@ package com.cccsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigBusApplication {
 
 	public static void main(String[] args) {
